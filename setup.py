685375bfa0ef356c21d571cf33b588c03f2b9b25
gpolaert <guillaume@logmatic.io>
2016-03-03 16:49:18 +0100

    add compatibility with python3

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 
 setup(
     name='logmatic-python',
-    version='0.0.9',
+    version='0.1',
     author='Logmatic.io support team',
     author_email='support@logmatic.io',
     packages = ['logmatic'],
     scripts=[],
     url='https://github.com/logmatic/logmatic-python',
-    download_url = 'https://github.com/logmatic/logmatic-python/tarball/0.0.9',
+    download_url = 'https://github.com/logmatic/logmatic-python/tarball/0.1',
     license='MIT',
     long_description=open('README.rst').read(),
     description='Python plugin to send logs to Logmatic.io',
-    install_requires = ['python-json-logger','logging'],
+    install_requires = ['python-json-logger'],
     keywords = ['logmatic']
 )

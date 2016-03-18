d1c8645b15e413771c3795466d7cd5cb1546abf9
Renaud Boutet <rb@focusmatic.com>
2016-03-18 16:25:33 +0100

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='logmatic-python',
-    version='0.1.4',
+    version='0.1.5',
     author='Logmatic.io support team',
     author_email='support@logmatic.io',
     packages = ['logmatic'],

2f39d366398f4b35355f97ff948a6afb80f6ab82
Guillaume Polaert <guillaume@logmatic.io>
2016-09-09 11:36:21 +0200

    upadte version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='logmatic-python',
-    version='0.1.5',
+    version='0.1.6',
     author='Logmatic.io support team',
     author_email='support@logmatic.io',
     packages = ['logmatic'],
     scripts=[],
     url='https://github.com/logmatic/logmatic-python',
-    download_url = 'https://github.com/logmatic/logmatic-python/tarball/0.1.4',
+    download_url = 'https://github.com/logmatic/logmatic-python/tarball/0.1.6',
     license='MIT',
     long_description=open('README.rst').read(),
     description='Python plugin to send logs to Logmatic.io',
